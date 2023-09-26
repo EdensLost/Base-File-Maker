e1044a085c768345bb8c84f5e942bb910f4eae5e
EdensLost <edendoesntexist@gmail.com>
2023-09-26 19:52:22 -0400

    Rewrote most of the questions

--- createNewBaseFile.py
+++ createNewBaseFile.py
@@ -6,46 +6,71 @@ def create():
     print("\n \n \nThis file adds a new file and is setup with the base requirements")
 
 
-    # Base text for the file
-# Name: <your name goes here – first and last minimum>
-# <ProgramName>.py
-#
-# Problem: <Brief, one or two sentence description of the
-# problem that this program solves, in your own
-# words.>
-# Certification of Authenticity:
-# <include one of the following>
-# I certify that this lab is entirely my own work.
-# I certify that this lab is my own work, but I
-# discussed it with: <Name(s)>
+    """Base text for the file
+Name: <your name goes here – first and last minimum>
+<ProgramName>.py
+
+Problem: <Brief, one or two sentence description of the
+problem that this program solves, in your own
+words.>
+Certification of Authenticity:
+<include one of the following>
+I certify that this lab is entirely my own work.
+I certify that this lab is my own work, but I
+discussed it with: <Name(s)>"""
 
 
     # Asks for the programs name
-    print("\nWhat is the name of the file (.py will be added)?")
-    fileName = input("Name of program = ")
+    fileName = input("\nWhat is the name of the file (.py will be added)? \nName of program = ")
 
     # Asks for the users name
-    print("\nWhat is your first and last name?")
-    userName = input("Name = ")
+    userName = input("\nWhat is your first and last name? \nName = ")
 
     # Asks for the programs purpose
-    print("\nWhat is the purpose of this program?\n(Start with: This program or similar)")
-    purpose = input("Purpose = ")
+    purpose = input("\nWhat is the purpose of this program?\n(Start with: This program or similar) \nPurpose = ")
 
     # Asks for the certification of the program
-    print("\nYes or no. Did you discuss the code with others?")
-    certi = input("Code discussed = ")
+    certi = input("\nYes or no. Did you discuss the code with others? \nCode discussed = ")
     certi.lower
     discussed = False
 
     # If the user did discuss with people then who
     if (certi == "yes"):
         discussed = True
-        print("\nWho were the people you discussed with (separate names with a comma and space)?")
-        peopleTalkedTo = input("\nNames = ")
+        peopleTalkedTo = input("\nWho were the people you discussed with (separate names with a comma and space)?\nNames = ")
+
+    # Asks if user wants to import anything
+    pickingImports = True
+
+    addImports = input("\nDo you wish to import libraries? (yes or no) \nImport = ")
+    addImports.lower
+
+    imports = []
+
+    # If user does want to import things user puts what to import
+    if (addImports == "yes"):
+        while (pickingImports == True):
+            try: importType = int(input("\nType 1 to do (import NAME)\nType 2 to do (import NAME as NEWNAME)\nType 3 to do (from NAME import *)\nType 4 to END"))
+            except: print("\nThat's not a valid import type")
+
+            if (importType == 1):
+                name = str(input("What is the name of the library?\nName = "))
+                imports.append(f"import {name}\n")
+
+            elif (importType == 2):
+                name = str(input("What is the name of the library?\nName = "))
+                newName = str(input("What is the new name for the library?\nName = "))
+                imports.append(f"import {name} as {newName}\n")
+
+            elif (importType == 3):
+                name = str(input("What is the name of the library?\nName = "))
+                imports.append(f"from {name} import *\n")
+
+            elif (importType == 4):
+                pickingImports = False
 
 
-
+            
 
     # Instantiates the new file
     newFile = None
@@ -57,20 +82,26 @@ def create():
         #except: "This file already exists"
 
         # Opens a possible new file but can also overwrite an already existing file
-        try: newFile = open(f"{fileName}.py", "w")
-        except: "This file already exists"
-        
+        newFile = open(f"{fileName}.py", "w")
+    
 
     # Writes the base to the new file
-    baseTextToWrite = ["## Imports:\n\n\n", f"# Name: {userName}\n#\n", f"# {fileName}.py\n#\n", f"# Problem: {purpose}\n#\n", f"# Certification of Authenticity:\n"]
+    importTextToWrite = ["## Imports\n"]
+    importTextToWrite.extend(imports)
+    importTextToWrite.append("\n\n\n")
+    newFile.writelines(importTextToWrite)
+
+    baseTextToWrite = [f"'''\nName: {userName}\n\n", f"{fileName}.py\n\n", f"Problem: {purpose}\n\n", f"Certification of Authenticity:\n"]
     newFile.writelines(baseTextToWrite)
     
     # Sets the certification
     if (discussed == True):
-        discussedText = [f"# I certify that this lab is my own work, but I\n", f"# discussed it with: {peopleTalkedTo}\n"]
+        discussedText = [f"I certify that this lab is my own work, but I\n", f"discussed it with: {peopleTalkedTo}\n"]
         newFile.writelines(discussedText)
     else:
-        newFile.write("# I certify that this lab is entirely my own work.\n")
+        newFile.write("I certify that this lab is entirely my own work.\n")
+
+    newFile.write("'''\n")
 
     newFile.writelines(["\n\ndef main():\n", "\t# Prints the purpose of the code\n", f"\tprint('{purpose}')\n\n", "\t#(Write your cool code here)\n\n", "\t# This can be deleted\n", "\treturn\n", "\nmain()"])
 
